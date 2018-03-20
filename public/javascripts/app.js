ad51d920120c4f34f4ca5db316089665330f4e15
Torey Heinz <torey@teagles.io>
2018-03-20 11:06:32 -0400

    Saved: 2018-03-19 19:52

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,6 +1,7 @@
+//= require vendor/autosize
+//= require vendor/foundation
 //= require vendor/jquery.sticky
 //= require vendor/what-input
-//= require vendor/foundation
 
 $(document).foundation();
 // $(".top-bar").sticky({zIndex:10});
@@ -11,3 +12,6 @@ $('nav#top-nav > ul ul').addClass('menu')
 $('header[data-background-url]').each(function() {
   $(this).css('background-image', 'url("' + $(this).data('background-url') + '")')
 })
+
+autosize($('textarea.autosize'));
+
